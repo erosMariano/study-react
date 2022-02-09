e77dbddc4bf10f05b23d76c49f3f373a1ba980d8
erosMariano <erosmariano1@gmail.com>
2022-02-09 16:39:44 -0300

    att

---
+++ usestates/src/App.tsx
@@ -0,0 +1,12 @@
+import Teset from "./components/Teset";
+
+function App() {
+	return (
+		<div>
+			<h1>Eros</h1>
+			<Teset />
+		</div>
+	);
+}
+
+export default App;
---
+++ usestates/src/components/Teset/index.tsx
@@ -0,0 +1,10 @@
+
+function Teset() {
+	return (
+		<>
+			<h1>Teset</h1>
+		</>
+	);
+}
+
+export default Teset;
